cafa50d969a813ca0992ea899a95e8b383a7709e
Vivek2018 <vivek2000@hotmail.com>
2018-10-18 18:43:47 -0500

    Flask with a working website

---
+++ Flask/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+     return render_template('Skeleton.html')
+
+
+if __name__ == '__main__': #starting function, causes the website to launch
+    app.run()
