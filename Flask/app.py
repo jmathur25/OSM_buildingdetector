99c1ce05c6e5f22b8b9a05076aadb8d294b46817
Vivek2018 <vivek2000@hotmail.com>
2018-11-02 03:10:02 -0500

    Flask with a working website + readin Search box, error working with flask on my pycharmm, but should work overall.

--- Flask/app.py
+++ Flask/app.py
@@ -1,11 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/readIn', methods = ['POST'])
 def hello_world():
+     result = request.form['search'] #reads in result from textbox
      return render_template('Skeleton.html')
+"""
+change return value to send result of the input box to openCV
+"""
 
 
 if __name__ == '__main__': #starting function, causes the website to launch
