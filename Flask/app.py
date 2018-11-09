9685eb904e8f0aea601626cadcdb9e0375413ac8
Vivek2018 <vivek2000@hotmail.com>
2018-11-08 18:22:24 -0600

    Copy code over into project, don't download the project -> leads to issues.

--- Flask/app.py
+++ Flask/app.py
@@ -1,15 +1,40 @@
-from flask import Flask, render_template, request, redirect
+
+
+
+""" from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/readIn', methods = ['POST'])
 def hello_world():
-     result = request.form['address'] #reads in result from textbox
-     render_template('Skeleton.html')
-     return result
+     username = request.form['username']
+     password = request.form['password']
+     render_template('Login.html')
+     return username, password
 
 
+
+if __name__ == '__main__': #starting function, causes the website to launch
+    app.run()
+"""
+
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def Login():
+     #username = request.form['username']
+     #password = request.form['password']
+     #render_template('Login.html')
+     #print(username + " " + password)
+     return render_template('Login.html')
+
+@app.route('/NewAccount.html/')
+def newAccount():
+    return render_template('/NewAccount.html')
 
 if __name__ == '__main__': #starting function, causes the website to launch
     app.run()
