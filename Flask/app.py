ff5577b5cb7aa2b92c887474e7b0240dcb6cc2c2
Vivek2018 <vivek2000@hotmail.com>
2018-11-08 18:28:18 -0600

    11/8 update

--- Flask/app.py
+++ Flask/app.py
@@ -1,24 +1,3 @@
-
-
-
-""" from flask import Flask, render_template, request, redirect
-
-app = Flask(__name__)
-
-
-@app.route('/readIn', methods = ['POST'])
-def hello_world():
-     username = request.form['username']
-     password = request.form['password']
-     render_template('Login.html')
-     return username, password
-
-
-
-if __name__ == '__main__': #starting function, causes the website to launch
-    app.run()
-"""
-
 from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
