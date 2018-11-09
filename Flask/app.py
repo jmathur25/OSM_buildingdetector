d0ac00ecb4b0acb92b09c676ff8bbf156de5e75f
Vivek2018 <vivek2000@hotmail.com>
2018-11-08 18:38:56 -0600

    Requests need to be fixed but aside from that everything is good

--- Flask/app.py
+++ Flask/app.py
@@ -11,9 +11,26 @@ def Login():
      #print(username + " " + password)
      return render_template('Login.html')
 
+@app.route('/', methods=['POST'])
+def LoginPost():
+    username = request.form['username']
+    password = request.form['password']
+    return username, password
+
 @app.route('/NewAccount.html/')
 def newAccount():
     return render_template('/NewAccount.html')
+
+@app.route('/NewAccount.html', methods=['POST'])
+def newAccountPost():
+    username = request.form['username']
+    password = request.form['password']
+    email = request.form['email']
+    name = request.form['name']
+    age = request.form['age']
+    address = request.form['address']
+    return username, password, email, name, age, address
+
 
 if __name__ == '__main__': #starting function, causes the website to launch
     app.run()
