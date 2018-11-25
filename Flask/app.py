06fe9b16342583fad037599401c3ffe4bf84bfd0
Vivek2018 <vivek2000@hotmail.com>
2018-11-24 22:00:42 -0600

    Added comments

--- Flask/app.py
+++ Flask/app.py
@@ -1,19 +1,19 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
-def login():
-   return render_template('login.html')
+@app.route('/') #base page that loads up on start/accessing the website
+def login():  #this method is called when the page starts up
+   return render_template('login.html') #displays the relevant page
 
-@app.route('/NewAccount.html')
+@app.route('/NewAccount.html') #activates when create a new account is clicked
 def newAccount():
-   return render_template('/NewAccount.html')
+   return render_template('/NewAccount.html') #links to the create a new account page
 
-@app.route('/test',methods = ['POST', 'GET'])
+@app.route('/test',methods = ['POST', 'GET'])   #this page is a test page to show if the information is saved
 def result():
-   if request.method == 'POST':
+   if request.method == 'POST': # if the user hits the submit button. post is called
       result = request.form
-      return render_template("test.html",result = result)
+      return render_template("test.html",result = result) #this links to the result page and dispalys the proper information
 
-if __name__ == '__main__':
+if __name__ == '__main__': #causes the program to boot
    app.run(debug = True)
