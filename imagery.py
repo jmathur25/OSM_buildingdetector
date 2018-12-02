c0d4b48b33aaf783e4489ebc226d5e0d6e20cf2f
Jim <jamesra2@illinois.edu>
2018-12-01 20:30:17 -0600

    Add imagery cache to speed up multiple imagery requests.

--- imagery.py
+++ imagery.py
@@ -5,6 +5,7 @@ import requests
 from PIL import Image
 from io import BytesIO
 from geolocation import *
+import os.path
 
 class ImageryDownloader(object):
 
@@ -16,9 +17,26 @@ class ImageryDownloader(object):
         """Downloads a map tile as an image.
            Note that x and y refer to Slippy Map coordinates.
         """
+        
+        # Try to fetch the image from the cache first
+        img_fname = "imgcache/" + str(zoom) + "/" + str(x) + "/" + str(y) + ".png"
+        if os.path.isfile(img_fname):
+            return Image.open(img_fname)
+        
+        # Download the image
         url = "https://a.tiles.mapbox.com/v4/digitalglobe.316c9a2e/" \
                "" + str(zoom) + "/" + str(x) + "/" + str(y) + "" \
                ".png?access_token=" + self.access_token
         req = requests.get(url)
         image = Image.open(BytesIO(req.content))
+        
+        # Save in the cache
+        if not os.path.exists(os.path.dirname(img_fname)):
+            try:
+                os.makedirs(os.path.dirname(img_fname))
+            except OSError as exc:
+                if exc.errno != errno.EEXIST:
+                    raise
+        image.save(img_fname)
+        
         return image
