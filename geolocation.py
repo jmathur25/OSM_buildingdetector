f7da4ff6ba7c9c1d749b81c7cdbfe4a2037cb401
Jim <jamesra2@illinois.edu>
2018-12-10 00:29:02 -0600

    Add geolocation functions to deal with tile matrices.

--- geolocation.py
+++ geolocation.py
@@ -36,6 +36,13 @@ def deg_to_tilexy(lat_deg, lon_deg, zoom):
         / math.pi) / 2.0 * n)
     return (int((x % 1) * 256), int((y % 1) * 256))
 
+def deg_to_tilexy_matrix(lat_deg, lon_deg, zoom, center_tile_xoffset, center_tile_yoffset):
+    """Converts geocoordinates to an x,y position on a tile matrix.
+    For a 3x3 tile matrix, the image used is the center tile, so pass
+    1 for center_tile_xoffset and 1 for center_tile_yoffset"""
+    x, y = deg_to_tilexy(lat_deg, lon_deg, zoom)
+    return (256 * center_tile_xoffset + x, 256 * center_tile_yoffset + y)
+
 def tilexy_to_deg(xtile, ytile, zoom, x, y):
     """Converts a specific location on a tile (x,y) to geocoordinates."""
     decimal_x = xtile + x / 256
@@ -45,3 +52,9 @@ def tilexy_to_deg(xtile, ytile, zoom, x, y):
     lat_rad = math.atan(math.sinh(math.pi * (1 - 2 * decimal_y / n)))
     lat_deg = math.degrees(lat_rad)
     return (lat_deg, lon_deg)
+
+def tilexy_to_deg_matrix(xtile, ytile, zoom, x, y, center_tile_xoffset, center_tile_yoffset):
+    """Converts an x,y position on a tile matrix to geocoordinates.
+    For a 3x3 tile matrix, the image used is the center tile, so pass
+    1 for center_tile_xoffset and 1 for center_tile_yoffset"""
+    return tilexy_to_deg(xtile + center_tile_xoffset, ytile + center_tile_yoffset, zoom, x, y)
