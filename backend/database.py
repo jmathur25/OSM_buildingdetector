b1716fbfbe31f41a277071a84a6ced5275441281
Jatin Mathur <jatinm2@illinois.edu>
2018-12-28 16:22:36 +0530

    cleaned out some old code, mostly in backend folder

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,8 @@
 # see https://dataset.readthedocs.io/en/latest/ for documentation on dataset
 import dataset
 
-# based on queries sent from frontend, backend will respond
 
 # call this to initialize backend
-# can put this in every function, or frontend can do it and pass to all the below functions
-# for now, I have all functions call it
 def init_backend():
     db = dataset.connect("sqlite:///mydatabase.db")
     table = db["users"]
@@ -61,15 +58,6 @@ def check_user_sign_in(username, password):
     else:
         return False
 
-# call this whenever a user queries something. The query will be added to the person's search history
-# for some reason, dataset doesn't like entries that are lists, so all search history code is commented out for now
-# def add_search_history(username=None, password=None, query=None):
-#     if check_user_exists(username, password) is False:
-#         return False
-#     table = init_backend()
-#     table.update({"Username": username, "Search History": table.find_one(Username=username)["Search History"].append(query)}, ["Username"])
-#     return True
-
 
 # call this to see the whole database
 def show_database():
@@ -81,15 +69,3 @@ def show_database():
 def clear_database():
     table = init_backend()
     table.drop()
-
-# create_user({"name": "Jatin",
-# "username": "jmather25",
-# "email": "jatinm2@illinois.edu",
-# "password": "fakepassword123",
-# "age": 18,
-# "address": "1234 Main Street",
-# "logins": 0})
-
-# show_database()
-# clear_database()
-# show_database()
